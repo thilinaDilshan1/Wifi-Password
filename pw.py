6f084230fa3fda02a9cfeb8d05802b60791f5c9c
Thilina Dilshan <tilinadilshan1999@gmail.com>
2023-03-30 17:53:59 +0530

    update

--- pw.py
+++ pw.py
@@ -1,10 +1,22 @@
 import subprocess
 
-meta_data = subprocess.check_output(['netsh','wlan','show','profiles'])
-data = meta_data.decode("utf-8")
-data = data.split("\n")
-names=[]
-for line in data:
-    if "All User Profile    : " in line:
-        name = line.split(":")[1]
-        names.append(name[1:-1])
+def get_wifi_profiles():
+    meta_data = subprocess.check_output(['netsh','wlan','show','profiles'])
+    data = meta_data.decode("utf-8")
+    data = data.split("\n")
+    names=[]
+    for line in data:
+        if "All User Profile     : " in line:
+            name = line.split(":")[1]
+            names.append(name[1:-1])
+    return names
+
+for name in get_wifi_profiles():
+    meta_data = subprocess.check_output(['netsh', 'wlan', 'show', 'profile',name,'key=clear'])
+    data = meta_data.decode("utf-8",errors="backslashreplace")
+    data = data.split("\n")
+    names = []
+    for line in data:
+        if "Key Content" in line:
+            password = line.split(":")[1]
+            print(name," : ",password)
